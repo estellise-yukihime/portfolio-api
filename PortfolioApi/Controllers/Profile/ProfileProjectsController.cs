36ce5fa72c908e49657acdad7f4e3771b959e726
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-07 23:45:40 +0800

    feat: consolidate ProfileProjectsController into ProfilesController with new endpoints for hero, social, and person data retrieval

--- PortfolioApi/Controllers/Profile/ProfileProjectsController.cs
+++
@@ -1,9 +0,0 @@
-using Asp.Versioning;
-using Microsoft.AspNetCore.Mvc;
-
-namespace PortfolioApi.Controllers.Profile;
-
-[ApiVersion(1)]
-[ApiController]
-[Route("api/v{version:apiVersion}profiles/{profileId}/projects")]
-public class ProfileProjectsController : ControllerBase;
--- PortfolioApi/Controllers/Profile/ProfilesController.cs
+++ PortfolioApi/Controllers/Profile/ProfilesController.cs
@@ -1,9 +1,75 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
+using PortfolioApi.DTO.Response;
+using PortfolioApi.Entities;
+using PortfolioApi.Services;
 
 namespace PortfolioApi.Controllers.Profile;
 
 [ApiVersion(1)]
 [ApiController]
 [Route("api/v{version:apiVersion}/profiles")]
-public class ProfilesController : ControllerBase;
+public class ProfilesController : ControllerBase
+{
+    private readonly IProfileService _profileService;
+
+    public ProfilesController(IProfileService profileService)
+    {
+        _profileService = profileService;
+    }
+
+    [HttpGet("{profileId:guid}")]
+    [ProducesResponseType(typeof(Entities.Profile), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfile(Guid profileId)
+    {
+        var profile = await _profileService.GetProfile(profileId);
+
+        if (profile is ProducesEntityFail<Entities.Profile>)
+        {
+            return Problem(statusCode: profile.StatusCode, title: profile.Error, detail: profile.Description);
+        }
+
+        return Ok(profile.Entity);
+    }
+
+    [HttpGet("{profileId:guid}/hero")]
+    [ProducesResponseType(typeof(ProfileHero), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfileHero(Guid profileId)
+    {
+        var profileHero = await _profileService.GetProfileHero(profileId);
+
+        if (profileHero is ProducesEntityFail<ProfileHero> fail)
+        {
+            return Problem(statusCode: fail.StatusCode, title: fail.Error, detail: fail.Description);
+        }
+
+        return Ok(profileHero.Entity);
+    }
+
+    [HttpGet("{profileId:guid}/social")]
+    [ProducesResponseType(typeof(ProfileSocial), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfileSocial(Guid profileId)
+    {
+        var profileSocial = await _profileService.GetProfileSocial(profileId);
+
+        return Ok(profileSocial.Entity);
+    }
+
+    [HttpGet("{profileId:guid}/person")]
+    [ProducesResponseType(typeof(ProfilePersonResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetProfilePerson(Guid profileId)
+    {
+        var profile = await _profileService.GetProfile(profileId);
+
+        if (profile is ProducesEntityFail<Entities.Profile>)
+        {
+            return Problem(statusCode: profile.StatusCode, title: profile.Error, detail: profile.Description);
+        }
+
+        return Ok(new ProfilePersonResponse(profile.Entity!));
+    }
+}
