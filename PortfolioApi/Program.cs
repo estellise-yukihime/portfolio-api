7179dd4e5d4c0cd66b139966b6e9529f9d913f5d
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-07 13:27:18 +0800

    feat: extract API versioning and JWT authentication setup into extension methods

--- PortfolioApi/Program.cs
+++ PortfolioApi/Program.cs
@@ -1,8 +1,5 @@
 using System.Text.Json;
-using Asp.Versioning;
 using PortfolioApi;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,37 +14,9 @@ builder.Services.AddControllers()
         x.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.SnakeCaseLower;
     });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(x =>
-    {
-        x.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = "portfolio",
-            ValidAudience = "portfolio-app",
-            IssuerSigningKey = new SymmetricSecurityKey("portfolio"u8.ToArray())
-        };
-    });
-
-builder.Services.AddApiVersioning(x =>
-{
-    x.DefaultApiVersion = new ApiVersion(1);
-    x.ReportApiVersions = true;
-    x.AssumeDefaultVersionWhenUnspecified = true;
-    x.ApiVersionReader = new UrlSegmentApiVersionReader();
-}).AddApiExplorer(x =>
-{
-    x.GroupNameFormat = "'v'V";
-    x.DefaultApiVersion = new ApiVersion(1);
-    x.ApiVersionParameterSource = new UrlSegmentApiVersionReader();
-    x.AssumeDefaultVersionWhenUnspecified = true;
-});
-
-builder.Services.AddHealthChecks();
-
+builder.Services.AddUrlVersioning();
 builder.Services.AddFluentMigration();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
--- PortfolioApi/ProgramExtensions.cs
+++ PortfolioApi/ProgramExtensions.cs
@@ -1,5 +1,8 @@
+using Asp.Versioning;
 using FluentMigrator.Runner;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Data.Sqlite;
+using Microsoft.IdentityModel.Tokens;
 using PortfolioApi.ExternalServices.Persistence;
 using PortfolioApi.ExternalServices.Persistence.Sqlite;
 
@@ -9,6 +12,44 @@ public static class ProgramExtensions
 {
     extension(IServiceCollection services)
     {
+        public IServiceCollection AddUrlVersioning()
+        {
+            services.AddApiVersioning(x =>
+            {
+                x.DefaultApiVersion = new ApiVersion(1);
+                x.ReportApiVersions = true;
+                x.AssumeDefaultVersionWhenUnspecified = true;
+                x.ApiVersionReader = new UrlSegmentApiVersionReader();
+            }).AddApiExplorer(x =>
+            {
+                x.GroupNameFormat = "'v'V";
+                x.DefaultApiVersion = new ApiVersion(1);
+                x.ApiVersionParameterSource = new UrlSegmentApiVersionReader();
+                x.AssumeDefaultVersionWhenUnspecified = true;
+            });
+
+            return services;
+        }
+
+        public IServiceCollection AddAuth()
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(x =>
+                {
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "portfolio",
+                        ValidAudience = "portfolio-app",
+                        IssuerSigningKey = new SymmetricSecurityKey("portfolio"u8.ToArray())
+                    };
+                });
+
+            return services;
+        }
+
         // note:
         //  we are using FluentMigrator as a migration tool to generate our database
         public IServiceCollection AddFluentMigration()
