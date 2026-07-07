38848748725047a89778ab0db98a61b36a1ea024
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-07 13:27:10 +0800

    feat: update User and Profile entities with refined properties and timestamps

--- PortfolioApi/Entities/Profile.cs
+++ PortfolioApi/Entities/Profile.cs
@@ -4,12 +4,13 @@ public class Profile
 {
     public int Id { get; set; }
     public int UserId { get; set; }
-    public string? ImgUrl { get; set; }
-    public string? Summary { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
-
-    public List<ProfileProject> Projects { get; set; } = [];
+    public string? Photo { get; set; }
+    public string? Title { get; set; }
+    public string? State { get; set; }
+    public string? Summary { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
 }
--- PortfolioApi/Entities/User.cs
+++ PortfolioApi/Entities/User.cs
@@ -3,12 +3,7 @@ namespace PortfolioApi.Entities;
 public class User
 {
     public int Id { get; set; }
-    public string? Username { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
     public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? DateOfBirth { get; set; }
-    public DateTime? CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
