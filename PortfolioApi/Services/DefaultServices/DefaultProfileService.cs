94007541686f96e7923e8fa0f503740b4e59dbe1
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-07 23:45:24 +0800

    feat: implement Profile retrieval methods in DefaultProfileService and update IProfileService interface

--- PortfolioApi/Services/DefaultServices/DefaultProfileService.cs
+++ PortfolioApi/Services/DefaultServices/DefaultProfileService.cs
@@ -6,16 +6,47 @@ namespace PortfolioApi.Services.DefaultServices;
 public class DefaultProfileService : IProfileService
 {
     private readonly IQueryProfile _queryProfile;
+    private readonly IQueryProfileHero _queryProfileHero;
+    private readonly IQueryProfileSocial _queryProfileSocial;
 
-    public DefaultProfileService(IQueryProfile queryProfile)
+    public DefaultProfileService(IQueryProfile queryProfile,
+        IQueryProfileHero queryProfileHero,
+        IQueryProfileSocial queryProfileSocial)
     {
         _queryProfile = queryProfile;
+        _queryProfileHero = queryProfileHero;
+        _queryProfileSocial = queryProfileSocial;
     }
 
-    public async Task<ProducesEntity<Profile>> InsertProfile(Profile profile)
+    public async Task<ProducesEntity<Profile>> GetProfile(Guid profileId)
     {
-        await _queryProfile.Insert(profile);
+        var profile = await _queryProfile.FindFromExternalId(profileId);
+
+        if (profile is null)
+        {
+            return new ProducesEntityFail<Profile>(StatusCodes.Status404NotFound, "Not Found", "No profile found");
+        }
 
         return new ProducesEntityGood<Profile>(profile);
+    }
+
+    public async Task<ProducesEntity<ProfileHero>> GetProfileHero(Guid profileId)
+    {
+        var profileHero = await _queryProfileHero.FindFromProfileExternalId(profileId);
+
+        if (profileHero is null)
+        {
+            return new ProducesEntityFail<ProfileHero>(StatusCodes.Status404NotFound, "Not Found",
+                "No profile hero found");
+        }
+
+        return new ProducesEntityGood<ProfileHero>(profileHero);
+    }
+
+    public async Task<ProducesEntity<List<ProfileSocial>>> GetProfileSocial(Guid profileId)
+    {
+        var profileSocial = await _queryProfileSocial.ListFromProfileExternalId(profileId);
+
+        return new ProducesEntityGood<List<ProfileSocial>>(profileSocial);
     }
 }
--- PortfolioApi/Services/IProfileService.cs
+++ PortfolioApi/Services/IProfileService.cs
@@ -1,6 +1,10 @@
+using PortfolioApi.Entities;
+
 namespace PortfolioApi.Services;
 
 public interface IProfileService
 {
-    
+    Task<ProducesEntity<Profile>> GetProfile(Guid profileId);
+    Task<ProducesEntity<ProfileHero>> GetProfileHero(Guid profileId);
+    Task<ProducesEntity<List<ProfileSocial>>> GetProfileSocial(Guid profileId);
 }
