3b396b09102ba3427495f51fc8630b50dffd32da
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-15 00:08:29 +0800

    feat: use configurable connection string for SQLite through IOptions

--- PortfolioApi/ExternalServices/Persistence/Sqlite/SqliteConnectionSource.cs
+++ PortfolioApi/ExternalServices/Persistence/Sqlite/SqliteConnectionSource.cs
@@ -1,11 +1,20 @@
 using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Options;
+using PortfolioApi.Options;
 
 namespace PortfolioApi.ExternalServices.Persistence.Sqlite;
 
 public class SqliteConnectionSource : IDbConnection<SqliteConnection>
 {
+    private readonly string _connectionSource;
+
+    public SqliteConnectionSource(IOptions<ConnectionSource> options)
+    {
+        _connectionSource = options.Value.Main ?? throw new NullReferenceException("The connection to the database is null");
+    }
+
     public SqliteConnection OpenConnection()
     {
-        return new SqliteConnection("Data Source=data/main.db");
+        return new SqliteConnection(_connectionSource);
     }
 }
