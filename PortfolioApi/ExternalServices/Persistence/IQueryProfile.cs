37300f41376cdd7a8e9120594b2b2646f17528f7
estellise-yukihime <shiranoyukihime@gmail.com>
2026-07-16 21:58:54 +0800

    feat: implement paginated query and additional profile lookup for QueryProfile interface

--- PortfolioApi/ExternalServices/Persistence/IQueryProfile.cs
+++ PortfolioApi/ExternalServices/Persistence/IQueryProfile.cs
@@ -1,10 +1,13 @@
 using PortfolioApi.Entities;
+using PortfolioApi.EntityValueObject;
 
 namespace PortfolioApi.ExternalServices.Persistence;
 
 public interface IQueryProfile
 {
+    Task<Paginated<Profile>> ListCardFromPage(Pagination pagination);
     Task<Profile?> FindFromExternalId(Guid uuid);
+    Task<Profile?> FindNaviFromExternalId(Guid uuid);
     Task Insert(Profile profile);
     Task Update(Profile profile);
     Task Delete(int id);
--- PortfolioApi/ExternalServices/Persistence/Sqlite/QueryProfile.cs
+++ PortfolioApi/ExternalServices/Persistence/Sqlite/QueryProfile.cs
@@ -1,6 +1,8 @@
 using Dapper;
 using Microsoft.Data.Sqlite;
 using PortfolioApi.Entities;
+using PortfolioApi.EntityValueObject;
+using PortfolioApi.Utilities;
 
 namespace PortfolioApi.ExternalServices.Persistence.Sqlite;
 
@@ -13,6 +15,70 @@ public class QueryProfile : IQueryProfile
         _dbConnection = dbConnection;
     }
 
+    public async Task<Paginated<Profile>> ListCardFromPage(Pagination pagination)
+    {
+        await using var connection = _dbConnection.OpenConnection();
+
+        var parametersForItems = new DynamicParameters();
+        var paginationBuilderForItems = new QueryPaginationBuilder(pagination, parametersForItems)
+            .ApplySearch()
+            .ApplySize()
+            .ApplyOffset()
+            .InsertSortDi()
+            .InsertSortBy("Id")
+            .InsertFilter("FirstName", "LastName", "Email");
+
+        var sqlItems = paginationBuilderForItems.Build("""
+                                                       select p.Id, p.ExternalId, p.FirstName, p.LastName, p.Email, 
+                                                              p.Title, p.Stack, p.State, p.About, p.CreatedAt, 
+                                                              p.UpdatedAt, psk.Id, psk.ProfileId, psk.Category, psk.Name, psk.Proficiency
+                                                       from Profile p 
+                                                       left join ProfileSkill psk on p.Id = psk.ProfileId 
+                                                       where (@Search is null or p.FirstName like ''%' || @Search || '%'' 
+                                                                  or p.LastName like ''%' || @Search || '%'' 
+                                                                  or p.Email like ''%' || @Search || '%'' 
+                                                                  or p.Title like ''%' || @Search || '%'') and 
+                                                           ($Filter) 
+                                                       order by $Sort $Direction
+                                                       limit @Size offset @Offset
+                                                       """);
+
+        var parametersForTotal = new DynamicParameters();
+        var paginationBuilderForTotal = new QueryPaginationBuilder(pagination, parametersForTotal)
+            .ApplySearch()
+            .InsertFilter("FirstName", "LastName", "Email");
+
+        var sqlTotal = paginationBuilderForTotal.Build("""
+                                                       select count(*)
+                                                       from Profile
+                                                       where (@Search is null or FirstName like ''%' || @Search || '%'' 
+                                                                  or LastName like ''%' || @Search || '%'' 
+                                                                  or Email like ''%' || @Search || '%'' 
+                                                                  or Title like ''%' || @Search || '%'') and
+                                                           ($Filter)
+                                                       """);
+
+        var items = await connection.QueryAsync<Profile, ProfileSkill, Profile>(sqlItems,
+            (p, sk) =>
+            {
+                p.Skills.Add(sk);
+
+                return p;
+            }, parametersForItems, splitOn: "Id");
+        var actual = items.GroupBy(p => p.Id)
+            .Select(g =>
+            {
+                var first = g.First();
+                first.Skills = g.Select(x => x.Skills.Single()).ToList();
+
+                return first;
+            })
+            .ToList();
+        var total = await connection.QuerySingleAsync<int>(sqlTotal, parametersForTotal);
+
+        return new Paginated<Profile>(actual, pagination.Page, pagination.Size, total);
+    }
+
     public async Task<Profile?> FindFromExternalId(Guid uuid)
     {
         await using var connection = _dbConnection.OpenConnection();
@@ -20,6 +86,21 @@ public class QueryProfile : IQueryProfile
         const string sql = """
                            select Id, UserId, FirstName, LastName, Email, Photo, Title, Stack, State, Summary, CreatedAt, UpdatedAt
                            from Profile
+                           where ExternalId = @ExternalId
+                           """;
+
+        return await connection.QueryFirstOrDefaultAsync<Profile>(sql, new { ExternalId = uuid.ToString() });
+    }
+
+    public async Task<Profile?> FindNaviFromExternalId(Guid uuid)
+    {
+        await using var connection = _dbConnection.OpenConnection();
+
+        const string sql = """
+                           select p.Id, p.ExternalId, p.FirstName, p.LastName, p.CV
+                           from Profile p
+                           inner join (select from ProfileCV where id in (select max(id) from ProfileCV group by ProfileId)) 
+                               as cv on p.Id = cv.ProfileId
                            where ExternalId = @ExternalId
                            """;
 
